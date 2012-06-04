01da22c68b968e5c8bbc130d62db2f75e06530fa
peterchou <peterchou139@gmail.com>
2012-06-04 20:35:57 +0800

    add documents

--- auth/models.py
+++ auth/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    use_gravt = models.BooleanField(default=True)
-    avt_uri = models.CharField(max_length=50)
+	id = models.AutoField(primary_key=True)
+	user = models.OneToOneField(User)
+	use_gravt = models.BooleanField(default=True)
+	avt_uri = models.CharField(max_length=50)
 # Create your models here.
