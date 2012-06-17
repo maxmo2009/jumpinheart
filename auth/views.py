2bee24671ca343a252c43f8b7cd7d0ebc1907114
peterchou <peterchou139@gmail.com>
2012-06-17 20:45:29 +0800

    inplement basic password check function

--- auth/views.py
+++ auth/views.py
@@ -6,50 +6,50 @@ from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
 import re
 def signup(request):
-	form = UserCreationForm()    
-	return render_to_response('auth/signup.html', {}, context_instance=RequestContext(request))
+  form = UserCreationForm()    
+  return render_to_response('auth/signup.html', {}, context_instance=RequestContext(request))
 
 def signup_form(request):
-	if request.method == 'POST':
-		email = request.POST['email']
-		pw = request.POST['pw']
-		username = 'peterchou139'
-		user = User.objects.create_user(username, email, pw)
-		user.save()
+  if request.method == 'POST':
+    email = request.POST['email']
+    pw = request.POST['pw']
+    username = 'peterchou139'
+    user = User.objects.create_user(username, email, pw)
+    user.save()
 	
-		login_user = authenticate(username=username, password=pw)
-		if login_user is not None:
-			return login_redirect(request, login_user)
+    login_user = authenticate(username=username, password=pw)
+    if login_user is not None:
+      return login_redirect(request, login_user)
 
 def logout_view(request):
-	logout(request)
-	return HttpResponseRedirect('/')
+  logout(request)
+  return HttpResponseRedirect('/')
 
 def login_view(request):
-	return render_to_response('auth/login.html', context_instance=RequestContext(request))
+  return render_to_response('auth/login.html', context_instance=RequestContext(request))
 
 def login_form(request):
-	if request.POST == 'POST':
-		email = request.POST['email']
-		pw = request.POST['pw']
-		try:
-			user = User.objects.get(email=email)
-			login_user = authenticate(username=user.username, password=pw)
-			if login_user is not None:
-				return login_redirect(request, login_user)
-		except User.DoesNotExist:
-			return HttpResponseRedirect('/')
+  if request.POST == 'POST':
+    email = request.POST['email']
+    pw = request.POST['pw']
+    try:
+      user = User.objects.get(email=email)
+      login_user = authenticate(username=user.username, password=pw)
+      if login_user is not None:
+        return login_redirect(request, login_user)
+    except User.DoesNotExist:
+      return HttpResponseRedirect('/')
 
 def login_redirect(request, login_user):
-	if login_user.is_active:
-		login(request, login_user)
-		return HttpResponseRedirect('/')
+  if login_user.is_active:
+    login(request, login_user)
+    return HttpResponseRedirect('/')
 def check_username(request):
   if request.is_ajax():
     if request.method == 'POST':
       username = request.POST['username']
       if len(username)>30 or len(username)<2:
-	    return HttpResponse('length error')#the user name is too long or too short
+        return HttpResponse('length error')#the user name is too long or too short
 
       if not full_match(username, '[a-zA-Z0-9_-]+'):
      	return HttpResponse('syntax error')#the user name contains improper letters
@@ -63,15 +63,15 @@ def check_email(request):
     if request.method == 'POST':
       email = request.POST['email']
       try:
-	    user = User.objects.get(email=email)
-	    return HttpResponse('not ok')
+        user = User.objects.get(email=email)
+        return HttpResponse('duplicate error')
       except User.DoesNotExist:
-	    return HttpResponse('ok')
+        return HttpResponse('OK')
 
 def full_match(string, restr):
   p = re.compile(restr)
   for i in range(0, len(string)):
     m = p.match(string, i)
     if m is None:
-	  return False
+      return False
   return True
--- static/js/functions.js
+++ static/js/functions.js
@@ -1,82 +1,112 @@
 jQuery(document).ajaxSend(function(event, xhr, settings) {
     function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
         }
-        return cookieValue;
+      }
+      return cookieValue;
     }
     function sameOrigin(url) {
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
+      // url could be relative or scheme relative or absolute
+      var host = document.location.host; // host + port
+      var protocol = document.location.protocol;
+      var sr_origin = '//' + host;
+      var origin = protocol + sr_origin;
+      // Allow absolute or scheme relative URLs to same origin
+      return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+             (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+             // or any other URL that isn't scheme relative or absolute i.e relative.
+             !(/^(\/\/|http:|https:).*/.test(url));
     }
     function safeMethod(method) {
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
 
     if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+      xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
     }
 });
 function check_username(input){
   username = input.value;
   var label = document.getElementById("label-username");
+
+  if (username.length > 30 || username.length < 2){
+    label.innerHTML = "User name can only have 2-30 characters";
+    return;
+  }
+
+  var re = /^[a-zA-Z0-9-_]{2,30}$/;
+  if (!re.test(username)){
+    label.innerHTML = "User name can only contain a-z, A-Z, 0-9, - and _";
+    return;
+  }
+
   var request = $.ajax({
-    url: "/check-username",
-    type: "POST",
-    data: {username: input.value},
-    dataType: "html"
+      url: "/check-username",
+      type: "POST",
+      data: {username: input.value},
+      dataType: "html"
   }); 
 
   request.done(function(msg){
-    if(msg == "OK"){
-      label.innerHTML = "OK";
-    }else if(msg == "syntax error"){
-      label.innerHTML = "User name must contain a-z, A-Z, 0-9, - and _";
-    }else if(msg == "length error"){
-      label.innerHTML = "User name can only have 2-30 characters";
-    }else if(msg == "duplicate error"){
-      label.innerHTML = "The user name has been token";
-    }
+      if(msg == "OK"){
+        label.innerHTML = "OK";
+      }else if(msg == "duplicate error"){
+        label.innerHTML = "The user name has been token";
+      }
   });
-  
+
   request.fail(function(jqXHR, textStatus){
-    label.innerHTML = "Connection error";
+      label.innerHTML = "Connection error";
   });
 }
 function check_email(input){
-  var request = $.ajax({
-    url: "/check-email",
-    type: "POST",
-    data: {email: input.value},
-    dataType: "html"
-  }); 
+  var email = input.value;
   var label = document.getElementById("label-email");
 
+  var re = /^([\w\d_\.-]+)@([\w\d_-]+\.)+\w{2,4}$/;
+  if (!re.test(email)){
+    label.innerHTML = "Invalid email";
+    return;
+  }
+
+  var request = $.ajax({
+      url: "/check-email",
+      type: "POST",
+      data: {email: email},
+      dataType: "html"
+  }); 
+
   request.done(function(msg){
-    if(msg == "ok"){
-      label.innerHTML = "OK";
-    }else if(msg == "not ok"){
-      label.innerHTML = "NOT OK";
-    }
+      if(msg == "OK"){
+        label.innerHTML = "OK";
+      }else if(msg == "duplicate error"){
+        label.innerHTML = "The email has been used";
+      }
   });
   request.fail(function(jqXHR, textStatus){
-    label.innerHTML = "Connection error";
-  });
+      label.innerHTML = "Connection error";
+  })
+}
+
+function check_pw(input){
+  var pw = input.value;
+  var label = document.getElementById("label-pw");
+  var pw_input = document.getElementById("register-password");
+  if (pw.length > 32 || pw.length < 6){
+    label.innerHTML = "Password can only have 6-32 characters";
+    pw_input.focus();
+    pw_input.select();
+    return;
+  }else{
+    label.innerHTML = "";
+  }
 }
